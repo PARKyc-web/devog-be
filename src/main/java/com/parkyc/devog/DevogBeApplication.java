a73e872c27ca14232fff65ef5495f41303086a6a
parkyc <erty1201@naver.com>
2026-04-08 23:00:04 +0900

    DEVOG init commit

---
+++ src/main/java/com/parkyc/devog/DevogBeApplication.java
@@ -0,0 +1,13 @@
+package com.parkyc.devog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkyc/devog/DevogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkyc.devog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
